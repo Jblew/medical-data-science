6238b284e4ffb906e40bbeac149dbba362df7809
Jblew <jedrzejblew@gmail.com>
2022-01-18 18:00:05 +0000

    Deploying to gh-pages from  @ 7352b236bb6581b6152ce1ae6079c16c29ee73eb 🚀

--- QALY.jl
+++ QALY.jl
@@ -10,11 +10,21 @@ md"""
 
 Barbara, 36-year old woman with failure of an internal organ. The disease reduces quality of her life by 50% related to full health. If she resignes from treatment she is expected to live only two years. After successful transplant the expected lifespan rises up to four years with 80% quality of life. The cost of the transplantation is 100k USD plus follow-up treatment (drugs, followup admissions and visits) is 10k/year. The risk of failure of the operation (transplant rejection and immediate death) is 15%.
 
-- Q1. *What is the cost of additional year of life withour quality-of-life correction?*
+- Q1. *What is the cost of additional year of life without quality-of-life correction?*
 - Q2. *What is the cost of one additional year of life with quality-of-life correction and a 5% discont rate?*
 """
 
 # ╔═╡ c6ecc03d-54a2-46ce-806c-c222e3b1dae6
+begin
+	disease_qol = 0.5
+	treatment_qol = 0.8
+	transplant_cost = 100*10^3
+end
+
+# ╔═╡ 4760fe27-e9c7-433f-861e-6ce166bf7e82
+
+
+# ╔═╡ 45a3bacb-4106-402d-b936-3b7f3fb80e2a
 
 
 # ╔═╡ 00000000-0000-0000-0000-000000000001
@@ -35,5 +45,7 @@ manifest_format = "2.0"
 # ╔═╡ Cell order:
 # ╠═f5d29a2c-17b8-4316-8b51-dc627aa61591
 # ╠═c6ecc03d-54a2-46ce-806c-c222e3b1dae6
+# ╠═4760fe27-e9c7-433f-861e-6ce166bf7e82
+# ╠═45a3bacb-4106-402d-b936-3b7f3fb80e2a
 # ╟─00000000-0000-0000-0000-000000000001
 # ╟─00000000-0000-0000-0000-000000000002
