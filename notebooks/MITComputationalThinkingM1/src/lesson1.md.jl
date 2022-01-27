1f0dd54a7507a253d8dc26269d9a3ee347869f0d
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-27 21:30:25 +0100

    fix: lesson 1 weave without md braces

--- notebooks/MITComputationalThinkingM1/src/lesson1.md.jl
+++ notebooks/MITComputationalThinkingM1/src/lesson1.md.jl
@@ -1,12 +1,10 @@
-```markdown
+
 # Lesson 1
 
 This is my learning notebook for the MIT's Computational Thinking course.
 Here is the link to the course for reference: https://computationalthinking.mit.edu/Spring21
 
 See [list of the notebook lessons](./index.html).
-
-```
 
 ```julia
 print("Hello, world!\n")
