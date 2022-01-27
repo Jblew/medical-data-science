93374cc4217addc21e36abd33cd81658af3b6fa7
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2022-01-27 21:12:44 +0100

    feat(MITComputationalThinkingM1): julia weave files separately

---
+++ notebooks/MITComputationalThinkingM1/src/MITComputationalThinkingM1.jl
@@ -0,0 +1,3 @@
+module MITComputationalThinkingM1
+
+end
--- notebooks/MITComputationalThinkingM1/src/build.jl
+++ notebooks/MITComputationalThinkingM1/src/build.jl
@@ -2,19 +2,22 @@ using Pkg
 
 project_dir = joinpath(@__DIR__, "..") |> realpath
 out_dir = out_path = "$(project_dir)/.out"
-in_file = ""
+in_files = [
+    "$(project_dir)/src/index.md.jl",
+    "$(project_dir)/src/lesson1.md.jl"
+]
 
 Pkg.activate(project_dir)
 Pkg.instantiate()
 
 using Weave
-weave(
-    "$(project_dir)/src/index.md.jl",
-    "$(project_dir)/src/lesson1.md.jl";
-    informat = "markdown",
-    doctype = "md2html",
-    out_path = out_dir,
-    keep_unicode = true,
-    fig_ext = ".svg"
-)
+for in_file in in_files
+    weave(in_file;
+        informat = "markdown",
+        doctype = "md2html",
+        out_path = out_dir,
+        keep_unicode = true,
+        fig_ext = ".svg"
+    )
+end
 mv("$(out_dir)/index.md.html", "$(out_dir)/index.html", force = true)
